75e1e4b5eae2f4d5c0d97386c7dcced5a4a2ab00
harissheikh8775 <harisbadboi@gmail.com>
2025-01-02 23:21:17 +0530

    sync js

---
+++ Async js/fileRead.js
@@ -0,0 +1,2 @@
+//import a file
+const fs = require("fs");
---
+++ Sync js/sumOfTwo.js
@@ -0,0 +1,7 @@
+//Sync function to find the sum of two numbers
+function sum(a, b) {
+    return a + b;
+}
+
+let ans = sum(2, 3);
+console.log("the sum is "+ ans);
