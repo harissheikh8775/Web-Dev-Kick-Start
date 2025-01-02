4d0b3fbe00b1db48611f98472a3f0ffdaac74032
harissheikh8775 <harisbadboi@gmail.com>
2025-01-02 23:26:13 +0530

    async js

--- Async js/fileRead.js
+++ Async js/fileRead.js
@@ -1,2 +1,8 @@
 //import a file
 const fs = require("fs");
+
+const content_A = fs.readFileSync("a.txt", "utf-8");
+console.log(content_A);
+
+const content_B = fs.readFileSync("b.txt", "utf-8");
+console.log(content_B);
